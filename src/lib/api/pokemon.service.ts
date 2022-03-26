5ad4883c079cd087fd5f1dc09e1145b3f65203c6
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2022-03-26 17:09:25 -0500

    Restore cache functionality

--- src/lib/api/pokemon.service.ts
+++ src/lib/api/pokemon.service.ts
@@ -1,8 +1,8 @@
-// import NodeCache from 'node-cache';
+import NodeCache from 'node-cache';
 import { POKEMON_URL, PAGE_SIZE } from '$lib/utils/constants';
 import type { PaginatedPokemonsResponse, PokemonResponse } from '$lib/pokemon.types';
 
-// const cache = new NodeCache({ stdTTL: 86400 });
+const cache = new NodeCache({ stdTTL: 86400 });
 
 export const fetchAllPokemons = async ({ page = 1 }: { page: number }) => {
 	const offset = (page - 1) * PAGE_SIZE;
@@ -14,16 +14,32 @@ export const fetchAllPokemons = async ({ page = 1 }: { page: number }) => {
 
 export const fetchPokemonByName = async ({ name }: { name: string }) => {
 	// Getting from cache
-	// const pokemonCache = cache.get<PokemonResponse>(name);
-	// if (pokemonCache !== undefined) {
-	// return pokemonCache;
-	// }
+	const pokemonCache = cache.get<PokemonResponse>(name);
+	if (pokemonCache !== undefined) {
+		return pokemonCache;
+	}
 
 	const pokemonResponse = await fetch(`${POKEMON_URL}/${name}`);
 	const pokemon: PokemonResponse = await pokemonResponse.json();
+	const cleanedPokemon = cleanPokemon(pokemon);
 
 	// Saving to cache
-	// cache.set(name, pokemon);
+	cache.set(name, cleanedPokemon);
 
-	return pokemon;
+	return cleanedPokemon;
+};
+
+const cleanPokemon = (pokemon: PokemonResponse) => {
+	return {
+		id: pokemon.id,
+		name: pokemon.name,
+		abilities: pokemon.abilities,
+		forms: pokemon.forms,
+		height: pokemon.height,
+		imageUrl: pokemon.imageUrl,
+		is_default: pokemon.is_default,
+		stats: pokemon.stats,
+		types: pokemon.types,
+		weight: pokemon.weight
+	};
 };
